48ab055583c92ad0f78c77b0be1695901f2233d4
qujinliang <qujin_liang@163.com>
2018-04-08 16:00:08 +0800

    "openpyxl_test 读取excel表格"

--- exa_100/openpyxl_test.py
+++ exa_100/openpyxl_test.py
@@ -3,7 +3,8 @@ import openpyxl
 
 wb = openpyxl.load_workbook('openpyxl.xlsx')
 sheet = wb['Sheet1']
-playload = {"fpdm":str(sheet['A2'].value)}
+playload = {"fpdm":str(sheet['A2'].value), "fphm":str(sheet['B2'].value), "fplx":str(sheet['C2'].value),
+            "fpje":(lambda x:"" if str(sheet['D2'].value) == 'None' else "")}
 print(playload)
 
 
