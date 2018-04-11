67ba6a339246e02517357743fbd558917f0a9f79
qujinliang <qujin_liang@163.com>
2018-04-11 18:37:04 +0800

    "contrast.py 对比两个接口返回的发票数据，并打印出不同的字段。 终点*根据字典的value找到Key"

--- exa_100/contrast.py
+++ exa_100/contrast.py
@@ -2,8 +2,8 @@ import requests
 import hashlib
 import json
 
-url = "http://company.fapiaoxx.com/companys/invoice/check"
-url2 = "http://check.fapiaoxx.com/invoice/check/"
+url = "http://check1.fapiaoxx.com/invoice/check/"
+url2 = "http://localhost:8000/invoice/check/"
 
 def md5(md5_str):
     m2 = hashlib.md5()
@@ -11,12 +11,12 @@ def md5(md5_str):
     return m2.hexdigest()
 
 data1 = {
-            "fpdm": "3100162130","fphm": "44564484","fplx": "01","fpje": "1198.11","kprq": "20170406","uniqueId": "1000004",
-            "sign": md5("fpdm=3100162130&fphm=44564484&fpje=1198.11&fplx=01&kprq=20170406""&uniqueId=1000004&de92dbf0b12d11e6aa28b0c090607876".encode("utf-8"))
+            "fpdm": "1100173320","fphm": "59993614","fplx": "04","jym":"743598","fpje": "","kprq": "20171227","uniqueId": "1000004",
+            "sign": md5("fpdm=1100173320&fphm=59993614&fpje=&fplx=04&jym=743598&kprq=20171227""&uniqueId=1000004&de92dbf0b12d11e6aa28b0c090607876".encode("utf-8"))
         }
 data2 ={
-            "fpdm": "3100162130","fphm": "44564484","fplx": "01","fpje": "1198.11","kprq": "20170406","uniqueId": "1000004",
-            "sign": md5("fpdm=3100162130&fphm=44564484&fpje=1198.11&fplx=01&kprq=20170406""&uniqueId=1000004&de92dbf0b12d11e6aa28b0c090607876".encode("utf-8"))
+            "fpdm": "1100173320","fphm": "59993614","fplx": "04","jym":"743598","fpje": "","kprq": "20171227","uniqueId": "1000004",
+            "sign": md5("fpdm=1100173320&fphm=59993614&fpje=&fplx=04&jym=743598&kprq=20171227""&uniqueId=1000004&de92dbf0b12d11e6aa28b0c090607876".encode("utf-8"))
         }
 r = requests.post(url, json = data1)
 r2 = requests.post(url2,json = data2)
@@ -24,11 +24,27 @@ r = r.json()
 r2 = r2.json()
 print(r)
 print(r2)
+
+
+def get_keys(d, value):
+    return [k for k, v in d.items() if v == value]
+
 for i,j in r2['data'].items():
-    if i in r['data'].keys() and j in r['data'].values():
-        pass
-    else:
-        print(i, j)
+        if i not in r['data'].keys() or j not in r['data'].values():
+            val = r['data'].get(i,'null')
+            print(i,j,':',get_keys(r['data'],val),val)
+
+
+
+
+
+
+
+
+
+
+
+
 
 a=1
 
