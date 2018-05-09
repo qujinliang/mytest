df794236c7543cf1fd66a2297799dbea8b48d502
qujinliang <qujin_liang@163.com>
2018-05-09 22:53:16 +0800

    'python高阶使用技巧2-3，统计序列中元素出现的频度'

---
+++ moocskill/MoocNum2/Demo2-3.py
@@ -0,0 +1,25 @@
+
+'''
+统计序列中元素出现的频度
+'''
+
+from collections import Counter
+from random import randint
+
+
+
+data = [randint(0,20) for i in range(30)]
+
+# 方法一 data做为键 0为值生成字典
+keys = data
+c = dict.fromkeys(keys,0)
+# 遍历键，并给每一个存在的键+1
+for x in keys:
+    c[x] +=1
+print(c)
+
+
+# 方法二 直接用Counter方法
+# most_common 方法统计出现最多的前3个
+d2 = Counter(data).most_common(3)
+print(d2)
